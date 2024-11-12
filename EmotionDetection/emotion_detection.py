fa3476a629b9e46a55f5057145e7f147d0b2757d
Marcia Leite <marciasilvaleite@hotmail.com>
2024-11-12 14:26:03 -0500

    Error handling and run static code analysis

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,9 @@
 import requests, json
 
 def emotion_detector(text_to_analyse):
+    if not text_to_analyse.strip():
+        return None
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
@@ -19,5 +22,19 @@ def emotion_detector(text_to_analyse):
             'sadness': emotion['sadness'],
             'dominant_emotion': dominant_emotion
         }
+    elif response.status_code == 400:
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion.items(), key=lambda x: x[1])[0]
+
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     else:
         return None
+
+    
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +9,19 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_emotion():
+    ''' Receives the text to be analyzed and returns the dominant emotion
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_analyze)
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    dominant_emotion = emotion_detector(text_to_analyze)
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again."
+    return f"The given text has been identified as {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
+    ''' Renders the main application Flask page
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
